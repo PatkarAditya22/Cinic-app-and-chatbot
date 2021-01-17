5c885b7197a1054cd2ba21777a2d42e90eedd6cd
PatkarAditya22 <adityapatkar22@gmail.com>
2021-01-17 13:51:18 +0530

    added few changes

--- app.js
+++ app.js
@@ -952,7 +952,7 @@ app.post('/chatBot', express.json(), (req, res) => {
 			return ((Number(a.loc.x) - Number(location.x)) ** 2 + (Number(a.loc.y) - Number(location.y)) ** 2) ** 0.5 -
 				((Number(b.loc.x) - Number(location.x)) ** 2 + (Number(b.loc.y) - Number(location.y)) ** 2) ** 0.5;
 		}).splice(0, 5);
-		const response = await fetch('http://742a07d9f3b6.ngrok.io/predictdisease', {
+		const response = await fetch('http://bc2257accf6a.ngrok.io/predictdisease', {
 			method: 'POST',
 			body: JSON.stringify({
 				symptoms: agent.context.get("symptoms").parameters["symptoms"].map(symptom => symptom.split(" ").join("_"))
@@ -1092,7 +1092,7 @@ app.post('/chatBot', express.json(), (req, res) => {
 			console.log(requestBody);
 			var responseData;
 
-			const response = await fetch("http://742a07d9f3b6.ngrok.io/suggestdiet", {
+			const response = await fetch("http://bc2257accf6a.ngrok.io/suggestdiet", {
 				method: "POST",
 				body: JSON.stringify(requestBody),
 				headers: {
